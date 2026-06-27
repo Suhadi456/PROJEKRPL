b7c774bb77213a4a6e7ff22329d12987a941ea88
Suhadi456 <prasetyakevin51@gmail.com>
2026-06-27 10:13:33 +0700

    Delete sipeternakanfinal/sipeternakan directory

--- sipeternakanfinal/sipeternakan/includes/auth_admin.php
+++
@@ -1,4 +0,0 @@
-<?php
-if (session_status() === PHP_SESSION_NONE) session_start();
-if (empty($_SESSION['user_id'])) { header('Location: ../login.php'); exit; }
-if ($_SESSION['role'] !== 'admin') { header('Location: ../login.php'); exit; }
